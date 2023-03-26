026fb3ed838173ec7b69fd006233633fbfed5953
khoicodesthings <khoitrinh@ou.edu>
2023-03-25 23:19:23 -0500

    Update code to not use scikit learn

--- logistic-reg.py
+++ logistic-reg.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 # Read in CSV file with song features and binary preference
 df = pd.read_csv('final.csv')
@@ -19,36 +17,47 @@ df = df.drop_duplicates()
 x = df.drop(columns=['name', 'preference'])
 y = df['preference']
 
+# Scale the features using mean normalization
+x = (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Split data into training and testing sets
+np.random.seed(5033)
+indices = np.random.permutation(len(df))
+train_indices, test_indices = indices[:int(0.8*len(df))], indices[int(0.8*len(df)):]
+
+x_train, y_train = x.iloc[train_indices], y.iloc[train_indices]
+x_test, y_test = x.iloc[test_indices], y.iloc[test_indices]
 
 # Define sigmoid function
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 # Define cost function
-def cost_function(X, y, theta):
+def cost_function(x, y, theta):
     m = len(y)
-    h = sigmoid(X.dot(theta))
+    h = sigmoid(x.dot(theta))
     eps = 1e-15 # small constant value to avoid taking the logarithm of zero
     J = -1/m * (y.T.dot(np.log(h + eps)) + (1-y).T.dot(np.log(1-h + eps)))
     return J
 
 # Define gradient function
-def gradient(X, y, theta):
+def gradient(x, y, theta):
     m = len(y)
-    h = sigmoid(X.dot(theta))
-    grad = 1/m * X.T.dot(h-y)
+    h = sigmoid(x.dot(theta))
+    grad = 1/m * x.T.dot(h-y)
     return grad
 
 # Define logistic regression function
-def logistic_regression(X, y, alpha, num_iters):
-    m, n = X.shape
+def logistic_regression(x, y, alpha, num_iters):
+    m, n = x.shape
     theta = np.zeros(n)
     J_history = []
     for i in range(num_iters):
-        cost = cost_function(X, y, theta)
-        grad = gradient(X, y, theta)
+        cost = cost_function(x, y, theta)
+        grad = gradient(x, y, theta)
         theta = theta - alpha * grad
         J_history.append(cost)
     return theta, J_history
@@ -56,17 +65,25 @@ def logistic_regression(X, y, alpha, num_iters):
 # Train logistic regression model
 alpha = 0.01
 num_iters = 1000
-theta, J_history = logistic_regression(X_train, y_train, alpha, num_iters)
+theta, J_history = logistic_regression(x_train, y_train, alpha, num_iters)
 
 # Make predictions
-y_pred = np.round(sigmoid(X_test.dot(theta)))
+y_pred = np.round(sigmoid(x_test.dot(theta)))
 
 # Extract song names from index
-song_names_test = song_names[X_test.index]
+song_names_test = song_names[x_test.index]
 
 # Save predictions to CSV file
-results = pd.DataFrame({'song_name': song_names_test, 'preference_pred': y_pred})
-results = results[['song_name', 'preference_pred']]
+#results = pd.DataFrame({'song_name': song_names_test, 'preference_pred': y_pred})
+#results = results[['song_name', 'preference_pred']]
+#results.to_csv('song_predictions.csv', index=False)
+
+# Extract correct preferences from the test set
+y_true = y_test.values
+
+# Save predictions and true preferences to CSV file
+results = pd.DataFrame({'song_name': song_names_test, 'preference_true': y_true, 'preference_pred': y_pred})
+results = results[['song_name', 'preference_true', 'preference_pred']]
 results.to_csv('song_predictions.csv', index=False)
 
 # Calculate precision and recall
@@ -88,5 +105,6 @@ print('Precision:', precision)
 print('Recall:', recall)
 
 # Evaluate model accuracy
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = np.sum(y_test == y_pred) / len(y_test)
 print('Accuracy:', accuracy)
+
